17756b414bddbd8802258be2cbd0e2a3b0509fbf
wyatex <wyatex@qq.com>
2026-01-20 14:31:01 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { isH5 } from '@uni-helper/uni-env'
+definePage(() => {
+  const title = isH5 ? 'H5 环境' : '非 H5 环境'
+  return {
+    title,
+    type: 'home',
+    layout: isH5 ? 'home' : 'default',
+  }
+})
+</script>
+
+<template>
+  <AppLogos />
+  <InputEntry />
+</template>
+
+<style></style>
